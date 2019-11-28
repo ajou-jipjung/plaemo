567a6d21a1f882fce99e233320d488d69b60dd4d
chj4535 <chj4535@ajou.ac.kr>
2019-11-28 10:56:00 +0900

    머지 오류로 인해서 이전 커밋으로 되돌림

--- app/src/main/java/com/po771/plaemo/PlaemoMemoList_Adapter.java
+++ app/src/main/java/com/po771/plaemo/PlaemoMemoList_Adapter.java
@@ -1,14 +1,11 @@
 package com.po771.plaemo;
 
-import android.content.Context;
 import android.util.Log;
-import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,12 +14,12 @@ import com.po771.plaemo.item.Item_memo;
 
 import java.util.List;
 
-public class PlemoMemoList_Adapter extends RecyclerView.Adapter<PlemoMemoList_Adapter.ViewHolder> {
+public class PlaemoMemoList_Adapter extends RecyclerView.Adapter<PlaemoMemoList_Adapter.ViewHolder> {
 
     private List<Item_memo> items;
 
 
-    public PlemoMemoList_Adapter(List<Item_memo> items) {
+    public PlaemoMemoList_Adapter(List<Item_memo> items) {
         this.items = items;
     }
 
@@ -30,7 +27,7 @@ public class PlemoMemoList_Adapter extends RecyclerView.Adapter<PlemoMemoList_Ad
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType){
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-        View itemView = inflater.inflate(R.layout.plemomemolist_item, viewGroup, false);
+        View itemView = inflater.inflate(R.layout.plaemomemolist_item, viewGroup, false);
 
         Log.d("qweqwe","qweqwe");
         return new ViewHolder(itemView);
@@ -78,7 +75,7 @@ public class PlemoMemoList_Adapter extends RecyclerView.Adapter<PlemoMemoList_Ad
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PlemoMemoList_Adapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(@NonNull PlaemoMemoList_Adapter.ViewHolder viewHolder, int position) {
         Item_memo item = items.get(position);
         viewHolder.imageView.setImageResource(R.drawable.book1);
         viewHolder.page_start.setText("p."+String.valueOf(item.getPage_start())+" ~ p."+String.valueOf(item.getPage_end()));
--- app/src/main/java/com/po771/plaemo/SplashActivity.java
+++ app/src/main/java/com/po771/plaemo/SplashActivity.java
@@ -1,94 +1,42 @@
 package com.po771.plaemo;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.po771.plaemo.DB.BaseHelper;
 import com.po771.plaemo.item.Item_book;
 import com.po771.plaemo.item.Item_folder;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 
 public class SplashActivity extends AppCompatActivity {
-
-    DataManager dataManager;
-
-    private final int PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         long startTime = System.currentTimeMillis();
 
-        if(request()){
-            initThing();
-            long endTime = System.currentTimeMillis();
+        initThing();
+        long endTime = System.currentTimeMillis();
 
-            long delayMax = 2000;
-            long delayTime = endTime - startTime;
-            if (delayMax > delayTime) {
-                try {
-                    Thread.sleep(delayMax - delayTime);
-                } catch (InterruptedException e) {
-                }
+        long delayMax = 2000;
+        long delayTime = endTime-startTime;
+        if(delayMax>delayTime){
+            try {Thread.sleep(delayMax-delayTime);} catch (InterruptedException e) {}
 
 
-            }
-            Intent intent = new Intent(this, PlaemoMainFolderActivity.class);
-            startActivity(intent);
-
-            finish();
         }
-        else{
-            finish();
-        }
+        Intent intent = new Intent(this, PlaemoMainFolderActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 
-    private boolean request(){
-        int permssionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (permssionCheck != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-        }
-        return true;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Intent intent = new Intent(this, PlaemoMainFolderActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    finish();
-                }
-                return;
-            }
-        }
-    }
 
     private void initThing(){
 
@@ -157,7 +105,7 @@ public class SplashActivity extends AppCompatActivity {
             item_book.setBook_star(1);
             item_book.setFolder("집교1/집교2");
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.book1);
-            saveToInternalStorage(bitmap,"1");
+            item_book.setImage_bitmap(bitmap);
             baseHelper.insertBook(item_book);
 
             item_book.setBook_name("집교책2");
@@ -168,7 +116,7 @@ public class SplashActivity extends AppCompatActivity {
             item_book.setBook_star(1);
             item_book.setFolder("집교1/집교2/HCI");
             bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.book2);
-            saveToInternalStorage(bitmap,"2");
+            item_book.setImage_bitmap(bitmap);
             baseHelper.insertBook(item_book);
 
             item_book.setBook_name("집교책3");
@@ -179,7 +127,7 @@ public class SplashActivity extends AppCompatActivity {
             item_book.setBook_star(1);
             item_book.setFolder("집교1/HCI");
             bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.book3);
-            saveToInternalStorage(bitmap,"3");
+            item_book.setImage_bitmap(bitmap);
             baseHelper.insertBook(item_book);
 
             item_book.setBook_name("집교책4");
@@ -190,36 +138,8 @@ public class SplashActivity extends AppCompatActivity {
             item_book.setBook_star(1);
             item_book.setFolder("집교1/집교2/HCI");
             bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.book4);
-            saveToInternalStorage(bitmap,"4");
+            item_book.setImage_bitmap(bitmap);
             baseHelper.insertBook(item_book);
         }
-        ///////////아직 사용 보류
-        dataManager=DataManager.getInstance();
-        dataManager.setBookList(baseHelper.getAllBook());
-        dataManager.setFolderList(baseHelper.getAllFolder());
-    }
-
-    private String saveToInternalStorage(Bitmap bitmapImage,String fileName){
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        // path to /data/data/yourapp/app_data/imageDir
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        // Create imageDir
-        File mypath=new File(directory,fileName+".jpg");
-
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(mypath);
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return directory.getAbsolutePath();
     }
 }
